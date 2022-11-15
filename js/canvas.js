0f4b01f71cf30a70c5b2a4791939d8aad50a5b15
alexandrminskiy <alexandrminskiy@gmail.com>
2022-11-15 14:18:21 +0300

    123

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,6 @@
 const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"];
 const BUBBLE_DENSITY = 100;
-
+        
 function generateDecimalBetween(left, right) {
     return (Math.random() * (left - right) + right).toFixed(2);
 }
@@ -8,15 +8,16 @@ function generateDecimalBetween(left, right) {
 class Bubble {
     constructor(canvas) {
         this.canvas = canvas;
+
         this.getCanvasSize();
         this.init();
     }
-
+  
     getCanvasSize() {
         this.canvasWidth = this.canvas.clientWidth;
         this.canvasHeight = this.canvas.clientHeight;
     }
-
+  
     init() {
         this.color = COLORS[Math.floor(Math.random() * COLORS.length)];
         this.size = generateDecimalBetween(1, 3);
@@ -27,24 +28,26 @@ class Bubble {
         this.movementX = generateDecimalBetween(-2, 2) / this.velocity;
         this.movementY = generateDecimalBetween(1, 20) / this.velocity;
     }
-
+  
     move() {
         this.translateX = this.translateX - this.movementX;
         this.translateY = this.translateY - this.movementY;
+
         if (this.translateY < 0 || this.translateX < 0 || this.translateX > this.canvasWidth) {
             this.init();
             this.translateY = this.canvasHeight;
         }
     }
- }
- 
+}
+
+
 class CanvasBackground {
     constructor(id) {
         this.canvas = document.getElementById(id);
-        this.ctx = this.canvas.getContext('2d');
+        this.ctx = this.canvas.getContext("2d");
         this.dpr = window.devicePixelRatio;
     }
-
+  
     start() {
         this.canvasSize();
         this.generateBubbles();
@@ -54,38 +57,35 @@ class CanvasBackground {
     canvasSize() {
         this.canvas.width = this.canvas.offsetWidth * this.dpr;
         this.canvas.height = this.canvas.offsetHeight * this.dpr;
+        
         this.ctx.scale(this.dpr, this.dpr);
+    }
+
+    animate() {
+        this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+
+        this.bubblesList.forEach((bubble) => {
+            bubble.move();
+            this.ctx.translate(bubble.translateX, bubble.translateY);
+            this.ctx.beginPath();
+            this.ctx.arc(0, 0, bubble.size, 0, 2 * Math.PI);
+            this.ctx.fillStyle = "rgba(" + bubble.color + "," + bubble.alpha + ")";
+            this.ctx.fill();
+            this.ctx.setTransform(this.dpr, 0, 0, this.dpr, 0, 0);
+        });
+
+        requestAnimationFrame(this.animate.bind(this));
     }
 
     generateBubbles() {
         this.bubblesList = [];
         for (let i = 0; i < BUBBLE_DENSITY; i++) {
-            this.bubblesList.push(new Bubble(this.canvas));
+            this.bubblesList.push(new Bubble(this.canvas))
         }
     }
 
-    animate() {
-        this.ctx.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
-        this.bubblesList.forEach((e) => {
-            e.move();
-            this.ctx.translate(e.translateX, e.translateY);
-            this.ctx.beginPath();
-            this.ctx.arc(0, 0, e.size, 0, 2 * Math.PI);
-            this.ctx.fillStyle = "rgba(" + e.color + "," + e.alpha + ")";
-            this.ctx.fill();
-            this.ctx.setTransform(this.dpr, 0, 0, this.dpr, 0, 0);
-            
-        });
-        requestAnimationFrame(this.animate.bind(this));
-    }
 }
 
-const animation = new CanvasBackground('orb-canvas');
-animation.start();
+const canvas = new CanvasBackground("orb-canvas");
 
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
+canvas.start();
---
+++ js/script.js
@@ -0,0 +1,14 @@
+s (6 sloc)  205 Bytes
+
+let number = Math.round(Math.random() * 100)
+
+if (number % 2 === 0) {
+    console.log(`${number} - чётное число`)
+} else {
+    console.log(`${number} - не чётное число`)
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
